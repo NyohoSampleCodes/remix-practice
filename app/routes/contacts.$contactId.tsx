cbfafe4483b4cd0d6a68afe588824ed9aec06c0b
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-12-15 15:29:56 +0900

    Apply validating params etc.
    https://remix.run/docs/en/main/start/tutorial#validating-params-and-throwing-responses

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,13 +1,21 @@
 import { json } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import invariant from "tiny-invariant";
 
 import type { ContactRecord } from "../data";
 
 import { getContact } from "../data";
 
-export const loader = async ({ params }) => {
+export const loader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  invariant(params.contactId, "missing contactId param");
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return json({ contact });
 };
 
