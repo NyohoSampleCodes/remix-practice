6c51c90e9dc1709ded079e98c8fc9945e5fc35b5
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-12-22 10:09:01 +0900

    Apply active link styling
    https://remix.run/docs/en/main/start/tutorial#active-link-styling

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import {
   Link,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -62,7 +63,16 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                            ? "pending"
+                            : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -74,7 +84,7 @@ export default function App() {
                       {contact.favorite ? (
                         <span>★</span>
                       ) : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
