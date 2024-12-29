1566f14ad31831d8c3ee8da3e651a8561c52d8ff
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-12-29 10:03:20 +0900

    Apply URLSearchParams and GET Submissions
    https://remix.run/docs/en/main/start/tutorial#urlsearchparams-and-get-submissions

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,10 @@ import {
   useLoaderData,
   useNavigation,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunctionArgs,
+} from "@remix-run/node";
 import appStylesHref from "./app.css?url";
 
 import { createEmptyContact, getContacts } from "./data";
@@ -20,8 +23,12 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 }
 
